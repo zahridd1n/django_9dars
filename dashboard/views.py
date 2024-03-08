765eac0037d0de00d69206a29d068816969e1890
zahridd1n <hatamovz047786@gmail.com>
2024-03-08 23:38:47 +0500

    3-commit dashboard qismini toliq tugatish,8-dars

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,7 +18,7 @@ def create_banner(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         body = request.POST.get('body')
-        file = request.POST.get('file')
+        file = request.FILES.get('file')
         models.Head.objects.create(
             title=title,
             description=body,
@@ -33,6 +33,32 @@ def list_banner(request):
         'banners':banners,
     }
     return render(request, 'dashboard/banner/list.html',context)
+
+def detail_banner(request, id):
+    banner = models.Head.objects.get(id=id)
+    context = {
+        'banner':banner,
+    }
+    return render(request, 'dashboard/banner/detail.html',context)
+
+
+def edit_banner(request, id):
+        banner = models.Head.objects.get(id=id)
+        if request.method == 'POST':
+            banner.title = request.POST['title']
+            banner.description = request.POST['body']
+            if request.FILES.get('banner_file'):
+                banner.slider_img = request.FILES.get('banner_file')
+            banner.save()
+
+        context = {
+            'banner':banner,
+        }
+        return render(request, 'dashboard/banner/edit.html',context)
+def delete_banner(request,id):
+   models.Head.objects.get(id=id).delete()
+   return redirect('list_banner')
+
 
 
 """Aboutus"""
@@ -54,12 +80,33 @@ def list_aboutus(request):
     }
     return render(request, 'dashboard/aboutus/list.html',context)
 
+def detail_aboutus(request, id):
+    about = models.About.objects.get(id=id)
+    context = {
+        'about':about,
+    }
+    return render(request, 'dashboard/aboutus/detail.html',context)
+
+def edit_aboutus(request, id):
+    about = models.About.objects.get(id=id)
+    if request.method == 'POST':
+        about.body = request.POST['aboutbody']
+        about.save()
+
+    context = {
+        'about':about,
+    }
+    return render(request, 'dashboard/aboutus/edit.html',context)
+
+def delete_aboutus(request,id):
+    models.About.objects.get(id=id).delete()
+    return redirect('list_aboutus')
 
 
 '''Price '''
 
 def create_price(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         price = request.POST['price']
         status = request.POST['status']
         body = request.POST['body']
@@ -78,19 +125,44 @@ def list_price(request):
 
     return render(request, 'dashboard/price/list.html',context)
 
+def detail_price(request, id):
+    price = models.Price.objects.get(id=id)
+    context = {
+        'price':price,
+    }
+    return render(request, 'dashboard/price/detail.html',context)
+
+
+def edit_price(request, id):
+    price = models.Price.objects.get(id=id)
+    if request.method == 'POST':
+        price.price = request.POST['price']
+        price.status = request.POST['status']
+        price.body = request.POST['body']
+        price.save()
+        return redirect('detail_price', price.id)
+    context = {
+        'price':price,
+    }
+
+    return render(request, 'dashboard/price/edit.html',context)
+
+def delete_price(request, id):
+    models.Price.objects.get(id=id).delete()
+    return redirect('list_price')
 
 
 """Service"""
 
 def create_service(request):
     if request.method == 'POST':
-        icon = request.POST.get('icon')
+        icon = request.FILES.get('icon')
         name = request.POST.get('name')
         body = request.POST.get('body')
         models.Service.objects.create(
             icon = icon,
             name = name,
-            body = body,
+            body = body
         )
     return render(request, 'dashboard/service/create.html')
 
@@ -101,3 +173,59 @@ def list_service(request):
        'services':services,
     }
     return render(request, 'dashboard/service/list.html',context)
+
+def detail_service(request, id):
+    service = models.Service.objects.get(id=id)
+    context = {
+       'service':service,
+    }
+    return render(request, 'dashboard/service/detail.html',context)
+
+
+def edit_service(request, id):
+    service = models.Service.objects.get(id=id)
+    if request.method == 'POST':
+        service.name = request.POST.get('name')
+        service.body = request.POST.get('body')
+        if request.FILES.get('icon'):
+            service.icon = request.FILES.get('icon')
+        service.save()
+        return redirect('detail_service', service.id)
+    context = {
+      'service':service,
+    }
+    return render(request, 'dashboard/service/edit.html',context)
+
+def delete_service(request, id):
+    models.Service.objects.get(id=id).delete()
+    return redirect('list_service')
+
+
+
+"""contact"""
+
+def list_contact(request):
+    contacts = models.Contact.objects.all()
+    context = {
+        'contacts':contacts,
+    }
+    return render(request, 'dashboard/contact/list.html',context)
+
+def detail_contact(request, id):
+    contact = models.Contact.objects.get(id=id)
+    context = {
+        'contact':contact,
+    }
+    return render(request, 'dashboard/contact/detail.html',context)
+
+def edit_contact(request, id):
+    contact = models.Contact.objects.get(id=id)
+    if request.method == 'POST':
+        contact.is_show = request.POST.get('is_show')=='on'
+        print('on')
+        contact.save()
+        return redirect('detail_contact', contact.id)
+    context = {
+        'contact':contact,
+    }
+    return render(request, 'dashboard/contact/edit.html',context)
--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Head(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100, null=True)
-    slider_img = models.ImageField(upload_to='Slider/')
+    slider_img = models.ImageField(upload_to='Slider/', null=True, blank=True)
 
     def __str__(self):
         return self.title
